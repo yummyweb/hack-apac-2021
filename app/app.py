2cbd00a9d4d05c61f9e621bf05861819bd480091
ozeliger <84307150+ozeliger@users.noreply.github.com>
2021-11-14 15:59:11 +0530

    Push changes

--- app/app.py
+++ app/app.py
@@ -56,7 +56,7 @@ def results():
 
         return render_template("results.html", flights=flights)
     except ResponseError as error:
-        print(error)
+        print(f"[Error] {error}")
         return render_template("results.html")
 
 @app.route("/hello")
